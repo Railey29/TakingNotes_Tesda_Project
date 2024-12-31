fb8b0510a0420cd694f8ff28501b7eb8c9622a2a
Railey Pacheco <111040098+Railey29@users.noreply.github.com>
2024-12-30 23:26:10 -0800

    Updated

---
+++ demo/src/main/java/com/example/DataManagment/DataManagmentInterface.java
@@ -0,0 +1,9 @@
+package com.example.DataManagment;
+
+import java.util.List;
+
+import com.example.UserManagment.User;
+
+public interface DataManagmentInterface {
+    List<User> AddNotes(User user);
+}
---
+++ demo/src/main/java/com/example/UserManagment/UserInterface.java
@@ -0,0 +1,7 @@
+package com.example.UserManagment;
+
+import java.util.List;
+
+public interface UserInterface {
+    List<String> getInputUser();
+}
